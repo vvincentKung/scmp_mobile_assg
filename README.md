0220b23d2b65eac007e032f7c43643dec1719e49
Vincent Kung <vincent.kung@nexplore.com>
2025-07-24 22:16:48 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# scmp_mobile_assg
+SCMP Mobile Technical Assignment
---
+++ android/app/src/main/kotlin/com/example/scmp_mobile_assg/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.scmp_mobile_assg
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
